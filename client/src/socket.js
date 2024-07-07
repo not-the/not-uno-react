b32f4965eab1570584189b49bf6fe6a22486fe77
not-the <87151784+not-the@users.noreply.github.com>
2024-07-06 22:08:22 -0400

    https

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = isProduction ?
-    "http://uno-api.notkal.com:8080" :              // Production endpoint
+    "https://uno-api.notkal.com:8080" :              // Production endpoint
     'http://localhost:8080'; // Development
 
 const socket = io(URL);
