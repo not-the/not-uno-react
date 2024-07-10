1e6791de1c488c3742d9f9bb36631724ec42a010
not-the <87151784+not-the@users.noreply.github.com>
2024-07-09 21:04:18 -0400

    Client dev server HTTPS

--- client/src/socket.js
+++ client/src/socket.js
@@ -7,6 +7,6 @@ const URL = isProduction ?
     "https://uno-api.notkal.com:8080" :              // Production endpoint
     'http://localhost:8080'; // Development
 
-const socket = io(URL);
+const socket = io(URL, { secure:true });
 
 export { socket, isProduction }
